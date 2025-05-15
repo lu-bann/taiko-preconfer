2523572416177999e2c5bcceb30f109fe95a3416
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-05-15 12:01:13 +0000

    Move base fee query function into taiko folder

--- crates/block_building/src/lib.rs
+++ crates/block_building/src/lib.rs
@@ -1,6 +1,3 @@
-pub mod base_fee;
 pub mod encode_util;
 pub mod taiko;
 pub mod verification;
-
-pub const L2_BLOCK_TIME_MS: u64 = 2000;
--- crates/block_building/src/taiko/mod.rs
+++ crates/block_building/src/taiko/mod.rs
@@ -1,5 +1,6 @@
 mod anchor;
 pub use anchor::create_anchor_transaction;
+pub mod base_fee;
 pub mod contracts;
 pub mod hekla;
 pub mod sign;
