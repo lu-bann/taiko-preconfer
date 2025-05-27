06804dd614c86e81406de99b765ae8ed6c793de9
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-05-27 15:26:34 +0000

    Replace usage of rpc client with trait

--- taiko-preconfer/src/rpc.rs
+++ taiko-preconfer/src/rpc.rs
@@ -1,16 +1,15 @@
 #![allow(dead_code)]
 
 use alloy_consensus::{Header, TxEnvelope};
-use alloy_json_rpc::RpcError;
 use alloy_primitives::{Address, Bytes};
-use alloy_rpc_client::RpcClient;
+use alloy_rpc_client::RpcClient as AlloyClient;
 use alloy_rpc_types::BlockNumberOrTag;
 use alloy_rpc_types_engine::JwtSecret;
-use alloy_transport::TransportErrorKind;
 use alloy_transport_http::{
     AuthLayer, Http, HyperClient,
     hyper_util::{client::legacy::Client, rt::TokioExecutor},
 };
+use block_building::http_client::{HttpClient, HttpError};
 use http_body_util::Full;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -24,12 +23,12 @@ const GET_HEADER_BY_NUMBER: &str = "eth_getHeaderByNumber";
 const TAIKO_TX_POOL_CONTENT: &str = "taikoAuth_txPoolContent";
 const TAIKO_TX_POOL_CONTENT_WITH_MIN_TIP: &str = "taikoAuth_txPoolContentWithMinTip";
 
-pub fn get_client(url: &str) -> PreconferResult<RpcClient> {
+pub fn get_client(url: &str) -> PreconferResult<AlloyClient> {
     let transport = Http::new(Url::parse(url)?);
-    Ok(RpcClient::new(transport, false))
+    Ok(AlloyClient::new(transport, false))
 }
 
-pub fn get_auth_client(url: &str, jwt_secret: JwtSecret) -> PreconferResult<RpcClient> {
+pub fn get_auth_client(url: &str, jwt_secret: JwtSecret) -> PreconferResult<AlloyClient> {
     let hyper_client = Client::builder(TokioExecutor::new()).build_http::<Full<Bytes>>();
     let auth_layer = AuthLayer::new(jwt_secret);
     let service = ServiceBuilder::new().layer(auth_layer).service(hyper_client);
@@ -37,23 +36,27 @@ pub fn get_auth_client(url: &str, jwt_secret: JwtSecret) -> PreconferResult<RpcC
     let layer_transport = HyperClient::with_service(service);
     let http_hyper = Http::with_client(layer_transport, Url::parse(url)?);
 
-    Ok(RpcClient::new(http_hyper, true))
+    Ok(AlloyClient::new(http_hyper, true))
 }
 
-pub async fn get_header(
-    client: &RpcClient,
+pub async fn get_header<Client: HttpClient>(
+    client: &Client,
     block_number: BlockNumberOrTag,
 ) -> PreconferResult<Header> {
     let params = json!([block_number]);
 
-    let rpc_call = client.request(GET_HEADER_BY_NUMBER, params.clone());
-    let method = rpc_call.method().to_string();
-    let params = rpc_call.request().params.clone();
-    let header: Option<Header> = rpc_call.await?;
-    header.ok_or(PreconferError::FailedRPCRequest { method, params })
+    let header: Option<Header> =
+        client.request(GET_HEADER_BY_NUMBER.to_string(), params.clone()).await?;
+    header.ok_or(PreconferError::FailedRPCRequest {
+        method: GET_HEADER_BY_NUMBER.to_string(),
+        params,
+    })
 }
 
-pub async fn get_header_by_id(client: &RpcClient, id: u64) -> PreconferResult<Header> {
+pub async fn get_header_by_id<Client: HttpClient>(
+    client: &Client,
+    id: u64,
+) -> PreconferResult<Header> {
     get_header(client, BlockNumberOrTag::Number(id)).await
 }
 
@@ -65,15 +68,15 @@ pub struct MempoolTxList {
     pub bytes_length: u64,
 }
 
-pub async fn get_mempool_txs(
-    client: &RpcClient,
+pub async fn get_mempool_txs<Client: HttpClient>(
+    client: &Client,
     beneficiary: Address,
     base_fee: u64,
     block_max_gas_limit: u64,
     max_bytes_per_tx_list: u64,
     locals: Vec<String>,
     max_transactions_lists: u64,
-) -> Result<Vec<MempoolTxList>, RpcError<TransportErrorKind>> {
+) -> Result<Vec<MempoolTxList>, HttpError> {
     let params = json!([
         beneficiary,
         base_fee,
@@ -82,13 +85,12 @@ pub async fn get_mempool_txs(
         locals,
         max_transactions_lists
     ]);
-    let rpc_call = client.request(TAIKO_TX_POOL_CONTENT, params);
-    rpc_call.await
+    client.request(TAIKO_TX_POOL_CONTENT.to_string(), params).await
 }
 
 #[allow(clippy::too_many_arguments)]
-pub async fn get_mempool_tx_with_min_tip(
-    client: &RpcClient,
+pub async fn get_mempool_tx_with_min_tip<Client: HttpClient>(
+    client: &Client,
     beneficiary: Address,
     base_fee: u64,
     block_max_gas_limit: u64,
@@ -96,7 +98,7 @@ pub async fn get_mempool_tx_with_min_tip(
     locals: Vec<String>,
     max_transactions_lists: u64,
     min_tip: u64,
-) -> Result<Vec<MempoolTxList>, RpcError<TransportErrorKind>> {
+) -> Result<Vec<MempoolTxList>, HttpError> {
     let params = json!([
         beneficiary,
         base_fee,
@@ -106,8 +108,7 @@ pub async fn get_mempool_tx_with_min_tip(
         max_transactions_lists,
         min_tip
     ]);
-    let rpc_call = client.request(TAIKO_TX_POOL_CONTENT_WITH_MIN_TIP, params);
-    rpc_call.await
+    client.request(TAIKO_TX_POOL_CONTENT_WITH_MIN_TIP.to_string(), params).await
 }
 
 pub fn flatten_mempool_txs(tx_lists: Vec<MempoolTxList>) -> Vec<TxEnvelope> {
