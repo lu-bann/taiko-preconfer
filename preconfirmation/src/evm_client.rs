4de012943eed9c04f2d6cd4a12969c082a0ab29d
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-06-03 12:40:30 +0000

    Move streams into separate folder

--- preconfirmation/src/evm_client.rs
+++
@@ -1,6 +0,0 @@
-use crate::http_client::HttpError;
-
-//#[cfg_attr(test, mockall::automock)]
-pub trait EvmClient {
-    fn get_nonce(&self, address: &str) -> impl Future<Output = Result<u64, HttpError>>;
-}
