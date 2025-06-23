2f1ca0fbd189012ea4404e81d9789884f3e4878d
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-06-23 14:41:55 +0000

    Fix holesky genesis timestamp

--- preconfirmation/src/slot_model.rs
+++ preconfirmation/src/slot_model.rs
@@ -4,7 +4,7 @@ use alloy_eips::merge::{EPOCH_DURATION, SLOT_DURATION};
 
 use crate::slot::Slot;
 
-pub const HOLESKY_GENESIS_TIMESTAMP: u64 = 1_695_902_100;
+pub const HOLESKY_GENESIS_TIMESTAMP: u64 = 1_695_902_400;
 pub const TAIKO_HOLESKY_GENESIS_TIMESTAMP: u64 = 1_711_697_940;
 
 #[derive(Debug)]
