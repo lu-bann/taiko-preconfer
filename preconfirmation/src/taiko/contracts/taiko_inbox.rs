ee78ed973cff2af514c723d6e25bb93fd5e23985
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-06-24 22:02:38 +0000

    Add header info stream for handling L2 reorgs

--- preconfirmation/src/taiko/contracts/taiko_inbox.rs
+++ preconfirmation/src/taiko/contracts/taiko_inbox.rs
@@ -1,9 +1,68 @@
 use alloy_sol_types::sol;
 
 sol!(
-     #[sol(rpc)]
+    #[derive(Debug, Default)]
+    #[sol(rpc)]
      contract TaikoInbox {
-        #[derive(Debug)]
+        struct BaseFeeConfig {
+            uint8 adjustmentQuotient;
+            uint8 sharingPctg;
+            uint32 gasIssuancePerSecond;
+            uint64 minGasExcess;
+            uint32 maxGasIssuancePerBlock;
+        }
+
+        struct BlockParams {
+            uint16 numTransactions;
+            uint8 timeShift;
+            bytes32[] signalSlots;
+        }
+
+        struct BlobParams {
+            bytes32[] blobHashes;
+            uint8 firstBlobIndex;
+            uint8 numBlobs;
+            uint32 byteOffset;
+            uint32 byteSize;
+            uint64 createdIn;
+        }
+
+        struct BatchParams {
+            address proposer;
+            address coinbase;
+            bytes32 parentMetaHash;
+            uint64 anchorBlockId;
+            uint64 lastBlockTimestamp;
+            bool revertIfNotFirstProposal;
+            BlobParams blobParams;
+            BlockParams[] blocks;
+        }
+
+        struct BatchInfo {
+            bytes32 txsHash;
+            BlockParams[] blocks;
+            bytes32[] blobHashes;
+            bytes32 extraData;
+            address coinbase;
+            uint64 proposedIn;
+            uint64 blobCreatedIn;
+            uint32 blobByteOffset;
+            uint32 blobByteSize;
+            uint32 gasLimit;
+            uint64 lastBlockId;
+            uint64 lastBlockTimestamp;
+            uint64 anchorBlockId;
+            bytes32 anchorBlockHash;
+            BaseFeeConfig baseFeeConfig;
+        }
+
+        struct BatchMetadata {
+            bytes32 infoHash;
+            address proposer;
+            uint64 batchId;
+            uint64 proposedAt;
+        }
+
         struct TransitionState {
             bytes32 parentHash;
             bytes32 blockHash;
@@ -13,16 +72,16 @@ sol!(
             uint48 createdAt;
         }
 
-        #[derive(Debug)]
         function getLastVerifiedTransition()
             external
             view
             returns (uint64 batchId_, uint64 blockId_, TransitionState memory ts_);
 
-        #[derive(Debug)]
         function getLastSyncedTransition()
             external
             view
             returns (uint64 batchId_, uint64 blockId_, TransitionState memory ts_);
+
+        event BatchProposed(BatchInfo info, BatchMetadata meta, bytes txList);
     }
 );
