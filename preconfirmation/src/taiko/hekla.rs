7e3e209edb9be265d118753d7a4988f5dc0b901b
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-06-26 19:38:30 +0000

    Fix hekla gas limit

--- preconfirmation/src/taiko/hekla.rs
+++ preconfirmation/src/taiko/hekla.rs
@@ -1,4 +1,4 @@
-pub const GAS_LIMIT: u64 = 241_000_000;
+pub const GAS_LIMIT: u64 = 240_000_000;
 
 use crate::taiko::contracts::TaikoAnchor;
 pub fn get_basefee_config_v2() -> TaikoAnchor::BaseFeeConfig {
