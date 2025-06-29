17ea2055f266683b48b17b1ff7181243996ab00e
lubkoll <11710767+lubkoll@users.noreply.github.com>
2025-06-29 09:37:52 +0000

    Move create_executable_data into TaikoL2Client.rs

--- preconfirmation/src/taiko/taiko_l2_client.rs
+++ preconfirmation/src/taiko/taiko_l2_client.rs
@@ -1,24 +1,24 @@
 use alloy_consensus::{Header, TxEnvelope};
 use alloy_contract::Error as ContractError;
 use alloy_json_rpc::RpcError;
-use alloy_primitives::FixedBytes;
-use alloy_primitives::{Address, ChainId, ruint::FromUintError};
+use alloy_primitives::{Address, Bytes, ChainId, FixedBytes, ruint::FromUintError};
 use alloy_provider::Provider;
 use alloy_provider::utils::Eip1559Estimation;
+use alloy_rlp::RlpEncodable;
 use alloy_rpc_types::{Header as RpcHeader, TransactionRequest};
 use alloy_rpc_types_engine::Claims;
 use alloy_transport::TransportErrorKind;
 use c_kzg::BYTES_PER_BLOB;
 use k256::ecdsa::{Error as EcdsaError, SigningKey};
 use libdeflater::CompressionError;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tracing::info;
 
+use crate::{compression::compress, util::pad_left};
+
 use crate::client::{
     HttpError, RpcClient, flatten_mempool_txs, get_latest_header, get_mempool_txs,
-};
-use crate::preconf::preconf_blocks::{
-    BuildPreconfBlockRequest, BuildPreconfBlockResponse, create_executable_data,
 };
 use crate::secret::Secret;
 use crate::taiko::{
@@ -287,4 +287,60 @@ impl TaikoL2Client {
         )?;
         Ok(format!("Bearer {jwt_token}"))
     }
+}
+
+#[derive(Debug, Serialize, RlpEncodable)]
+#[serde(rename_all = "camelCase")]
+pub struct ExecutableData {
+    parent_hash: FixedBytes<32>,
+    fee_recipient: Address,
+    block_number: u64,
+    gas_limit: u64,
+    timestamp: u64,
+    transactions: Bytes,
+    extra_data: Bytes,
+    base_fee_per_gas: u64,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BuildPreconfBlockRequest {
+    pub executable_data: ExecutableData,
+    pub end_of_sequencing: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BuildPreconfBlockResponse {
+    pub block_header: RpcHeader,
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn create_executable_data(
+    base_fee_per_gas: u64,
+    block_number: u64,
+    sharing_percentage: u8,
+    fee_recipient: Address,
+    gas_limit: u64,
+    parent_hash: FixedBytes<32>,
+    timestamp: u64,
+    txs: Vec<TxEnvelope>,
+) -> Result<ExecutableData, CompressionError> {
+    info!(
+        "tx hash {}",
+        alloy_primitives::keccak256(alloy_rlp::encode(&txs))
+    );
+    let tx_bytes = compress(txs)?;
+    let transactions = Bytes::from(tx_bytes);
+
+    Ok(ExecutableData {
+        base_fee_per_gas,
+        block_number,
+        extra_data: pad_left::<32>(&[sharing_percentage]),
+        fee_recipient,
+        gas_limit,
+        parent_hash,
+        timestamp,
+        transactions,
+    })
 }
